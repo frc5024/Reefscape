9a26fff4d24685f4160afe02f5eb5b6988324905
Ro-vdv <ronanvdvlag@hotmail.com>
2025-01-09 14:28:54 -0500

    \

---
+++ src/main/java/frc/robot/SwerveModule.java
@@ -0,0 +1,117 @@
+package frc.robot;
+
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.lib.math.Conversions;
+import frc.lib.util.SwerveModuleConstants;
+
+public class SwerveModule {
+    public int moduleNumber;
+    private Rotation2d angleOffset;
+
+    /* used to track drive acceleration */
+    private double driveAccelerationMPS = 0.0;
+    private double drivePreviousTimestamp = 0.0;
+    private double drivePreviousVelocityMPS = 0.0;
+
+    private TalonFX mAngleMotor;
+    private TalonFX mDriveMotor;
+    private CANcoder angleEncoder;
+
+    private final SimpleMotorFeedforward driveFeedForward = new SimpleMotorFeedforward(Constants.Swerve.driveKS,
+            Constants.Swerve.driveKV, Constants.Swerve.driveKA);
+
+    /* drive motor control requests */
+    private final DutyCycleOut driveDutyCycle = new DutyCycleOut(0);
+    private final VelocityVoltage driveVelocity = new VelocityVoltage(0);
+
+    /* angle motor control requests */
+    private final PositionVoltage anglePosition = new PositionVoltage(0);
+
+    public SwerveModule(int moduleNumber, SwerveModuleConstants moduleConstants) {
+        this.moduleNumber = moduleNumber;
+        this.angleOffset = moduleConstants.angleOffset;
+
+        /* Angle Encoder Config */
+        angleEncoder = new CANcoder(moduleConstants.cancoderID);
+        angleEncoder.getConfigurator().apply(Robot.ctreConfigs.swerveCANcoderConfig);
+
+        /* Angle Motor Config */
+        mAngleMotor = new TalonFX(moduleConstants.angleMotorID);
+        mAngleMotor.getConfigurator().apply(Robot.ctreConfigs.swerveAngleFXConfig);
+        resetToAbsolute();
+
+        /* Drive Motor Config */
+        mDriveMotor = new TalonFX(moduleConstants.driveMotorID);
+        mDriveMotor.getConfigurator().apply(Robot.ctreConfigs.swerveDriveFXConfig);
+        mDriveMotor.getConfigurator().setPosition(0.0);
+    }
+
+    public void setDesiredState(SwerveModuleState desiredState, boolean isOpenLoop) {
+        desiredState = SwerveModuleState.optimize(desiredState, getState().angle);
+        mAngleMotor.setControl(anglePosition.withPosition(desiredState.angle.getRotations()));
+        setSpeed(desiredState, isOpenLoop);
+    }
+
+    private void setSpeed(SwerveModuleState desiredState, boolean isOpenLoop) {
+        if (isOpenLoop) {
+            driveDutyCycle.Output = desiredState.speedMetersPerSecond / Constants.Swerve.maxSpeed;
+            mDriveMotor.setControl(driveDutyCycle);
+        } else {
+            driveVelocity.Velocity = Conversions.MPSToRPS(desiredState.speedMetersPerSecond,
+                    Constants.Swerve.wheelCircumference);
+            driveVelocity.FeedForward = driveFeedForward.calculate(desiredState.speedMetersPerSecond);
+            mDriveMotor.setControl(driveVelocity);
+        }
+    }
+
+    public Rotation2d getCANcoder() {
+        return Rotation2d.fromRotations(angleEncoder.getAbsolutePosition().getValue());
+    }
+
+    public void resetToAbsolute() {
+        double absolutePosition = getCANcoder().getRotations() - angleOffset.getRotations();
+        mAngleMotor.setPosition(absolutePosition);
+    }
+
+    public SwerveModuleState getState() {
+        return new SwerveModuleState(
+                Conversions.RPSToMPS(mDriveMotor.getVelocity().getValue(), Constants.Swerve.wheelCircumference),
+                Rotation2d.fromRotations(mAngleMotor.getPosition().getValue()));
+    }
+
+    public SwerveModulePosition getPosition() {
+        return new SwerveModulePosition(
+                Conversions.rotationsToMeters(mDriveMotor.getPosition().getValue(),
+                        Constants.Swerve.wheelCircumference),
+                Rotation2d.fromRotations(mAngleMotor.getPosition().getValue()));
+    }
+
+    /**
+     * Called from Swerve Drive subsystem periodic
+     */
+    public void update() {
+        SmartDashboard.putNumber("Mod " + moduleNumber + " CANcoder", getCANcoder().getDegrees());
+        SmartDashboard.putNumber("Mod " + moduleNumber + " Angle", getPosition().angle.getDegrees());
+        SmartDashboard.putNumber("Mod " + moduleNumber + " Velocity", getState().speedMetersPerSecond);
+
+        // Log module data to AK
+        double timeBetweenUpdates = Timer.getFPGATimestamp() - this.drivePreviousTimestamp;
+        double currentVelocityMPS = getState().speedMetersPerSecond;
+
+        this.driveAccelerationMPS = (currentVelocityMPS - this.drivePreviousVelocityMPS) / timeBetweenUpdates;
+        this.drivePreviousVelocityMPS = currentVelocityMPS;
+        this.drivePreviousTimestamp = Timer.getFPGATimestamp();
+
+    }
+}
