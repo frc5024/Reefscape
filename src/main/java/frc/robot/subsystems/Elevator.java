fcd6ee759326c676cd16b2c7027e7dd96cdfeb16
Seankariya <sean.kariya@gmail.com>
2025-01-16 15:59:10 -0500

    new branch

---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase {
+    private TalonSRX elevatorMotor;
+    
+    public Elevator() {
+        
+
+    }
+
+
+}
+
+
+
+
