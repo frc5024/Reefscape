ae057eb1b89a037ba80c6b058d26c9f056ae4bc8
wilbur <unnamed.wphillips@gmail.com>
2025-02-01 11:03:57 -0500

    moved ultrasonic code to climb subsystem

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -9,32 +9,43 @@ import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class Climb extends SubsystemBase {
+    private static Climb mInstance = null;
+
     private TalonFX climbMotor;
 
     // private DigitalInput limitSwitch;
-    private final MedianFilter m_filter;
     // private final DigitalInput kUltrasonicEchoPort;
     // private final DigitalOutput kUltrasonicPingPort;
     // private final Ultrasonic m_ultrasonic;
 
     ShuffleboardTab tab = Shuffleboard.getTab("Climb");
     GenericEntry climbSpeed = tab.add("climbSpeed", .35).getEntry();
-    
 
+    private final Ultrasonic m_ultrasonic = new Ultrasonic(5, 4);
+    double distanceMillimetres;
+    MedianFilter filter = new MedianFilter(5);
 
+    public static Climb getInstance() {
+        if (mInstance == null) {
+            mInstance = new Climb();
+        }
+        return mInstance;
+    }
 
-    public Climb() {
+    private Climb() {
         climbMotor = new TalonFX(7);
         // kUltrasonicEchoPort = new DigitalInput(8);
         // kUltrasonicPingPort = new DigitalOutput(9);
         // m_ultrasonic = new Ultrasonic(9, 8);
-        m_filter = new MedianFilter(5);
-        tab.addDouble("encoder value", () ->climbMotor.getPosition().getValueAsDouble());
+        tab.addDouble("encoder value", () -> climbMotor.getPosition().getValueAsDouble());
+
+        Ultrasonic.setAutomaticMode(true);
         // linebreak = new DigitalInput(7);
         // limitSwitch = new DigitalInput(1);
     }
@@ -57,8 +68,18 @@ public class Climb extends SubsystemBase {
     }
 
     @Override
-    public void periodic(){
-        
-    }
+    public void periodic() {
+        distanceMillimetres = m_ultrasonic.getRangeMM();
+        double measurement = filter.calculate(distanceMillimetres);
+        boolean overThreshold;
 
+        if (measurement >= 100) {
+            overThreshold = true;
+        } else {
+            overThreshold = false;
+        }
+
+        SmartDashboard.putBoolean("Over Threshold", overThreshold);
+        SmartDashboard.putNumber("Ultrasonic", measurement);
+    }
 }
