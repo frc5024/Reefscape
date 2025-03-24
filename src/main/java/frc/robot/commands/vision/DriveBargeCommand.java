cd7e05dd04d61d14aa9a79472c334f156a6aadcc
John Lownie <jlownie@rogers.com>
2025-03-24 10:58:05 -0400

    sim update

--- src/main/java/frc/robot/commands/vision/DriveBargeCommand.java
+++ src/main/java/frc/robot/commands/vision/DriveBargeCommand.java
@@ -6,7 +6,6 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathPlannerPath;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.SwerveDriveSubsystem;
 
 /**
@@ -15,7 +14,6 @@ import frc.robot.subsystems.SwerveDriveSubsystem;
 public class DriveBargeCommand extends Command {
     private final SwerveDriveSubsystem swerveDriveSubsystem;
 
-    private Command commandGroup;
     private Command followPathCommand;
 
     /**
@@ -29,53 +27,50 @@ public class DriveBargeCommand extends Command {
 
     @Override
     public void end(boolean interrupted) {
-        super.end(interrupted);
-        if (this.commandGroup != null)
-            this.commandGroup.cancel();
-        this.swerveDriveSubsystem.resetDrivePID();
+        if (this.followPathCommand != null) {
+            Logger.recordOutput("Commands/Active Command", "");
+            this.followPathCommand.end(interrupted);
+        }
 
-        Logger.recordOutput("Commands/Active Command", "");
+        this.swerveDriveSubsystem.resetDrivePID();
     }
 
     @Override
     public void execute() {
+        if (this.followPathCommand != null) {
+            this.followPathCommand.execute();
+        }
     }
 
     @Override
     public void initialize() {
         // zero drive pid since we are driving closed loop
-        this.swerveDriveSubsystem.zeroDrivePID();
 
-        schedulePathCommand();
+        this.followPathCommand = getfollowPathCommand();
+        if (this.followPathCommand != null) {
 
-        Logger.recordOutput("Commands/Active Command", this.getName());
+            this.followPathCommand.initialize();
+            Logger.recordOutput("Commands/Active Command", this.getName());
+        }
     }
 
     @Override
     public boolean isFinished() {
-        try {
-
-            return this.followPathCommand.isFinished();
-
-        } catch (Exception e) {
-            return true;
-        }
+        return this.followPathCommand != null ? this.followPathCommand.isFinished() : true;
     }
 
     /**
      * 
      */
-    private void schedulePathCommand() {
+    private Command getfollowPathCommand() {
         try {
 
             PathPlannerPath pathPlannerPath = PathPlannerPath.fromPathFile("DriveBarge - Path");
 
-            this.followPathCommand = AutoBuilder.pathfindThenFollowPath(pathPlannerPath,
-                    frc.robot.autonomous.AutoBuilder.CONSTRAINTS);
-            this.commandGroup = Commands.sequence(this.followPathCommand);
-            this.commandGroup.schedule();
+            return AutoBuilder.pathfindThenFollowPath(pathPlannerPath, frc.robot.autonomous.AutoBuilder.CONSTRAINTS);
 
         } catch (Exception e) {
+            return null;
         }
     }
 }
